ae33b9b7352706274c293e5314a821c003797d7f
09Kathy <kathymdooley@gmail.com>
2020-12-21 13:22:35 -0600

    Issue #2 - layer toggles

--- src/app/mainview/map/map.component.ts
+++ src/app/mainview/map/map.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {MapService} from '../../shared/services/map.service';
-import {Map} from 'leaflet';
+import { MapService } from '../../shared/services/map.service';
+import { Map } from 'leaflet';
 import * as L from 'leaflet';
 import * as esri from 'esri-leaflet';
 
 @Component({
   selector: 'app-mainview-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.less']
+  styleUrls: ['./map.component.less'],
 })
 export class MapComponent implements OnInit {
   collapsedMap: any;
   collapsedDataPanel: any;
   legendExpanded = true;
 
-  constructor(private _mapService: MapService,) {}
+  constructor(private _mapService: MapService) {}
 
   ngOnInit() {
     this.expandCollapseDataPanel();
@@ -22,19 +22,19 @@ export class MapComponent implements OnInit {
     this._mapService.map = L.map('map', {
       center: L.latLng(45.0522, -82.4846), //[-82.4846, 45.0522] - GL Geo Center
       zoom: 6,
-      renderer: L.canvas()
+      renderer: L.canvas(),
     });
 
-    this._mapService.map.addLayer(this._mapService.baseMaps[this._mapService.chosenBaseLayer]);
-    this.addLengend();
+    this._mapService.map.addLayer(
+      this._mapService.baseMaps[this._mapService.chosenBaseLayer]
+    );
+    this.addLegend();
 
     //this._mapService.map.addLayer(this._mapService.auxLayers);
-    
-    //this._mapService.basinLayer.addTo(this._mapService.map);
-    this._mapService.map.addLayer(this._mapService.basinLayer['basin']);
-    //L.control.layers(this._mapService.overlayLayers).addTo(this._mapService.map)
-    
 
+    //this._mapService.basinLayer.addTo(this._mapService.map);
+    this._mapService.map.addLayer(this._mapService.auxLayers['basin']);
+    //L.control.layers(this._mapService.overlayLayers).addTo(this._mapService.map)
   }
 
   expandCollapseDataPanel() {
@@ -47,45 +47,45 @@ export class MapComponent implements OnInit {
     // When data or map is collapsed or expanded,
     // invalidate size to refresh tiles and center map
     setTimeout(() => {
-        this._mapService.map.invalidateSize()
+      this._mapService.map.invalidateSize();
     }, 150);
-  };
+  }
 
-  addLengend() {
-    this._mapService.legend = new L.Control({position: 'bottomright'});
-    this._mapService.legend.onAdd = function (){
-      const div = L.DomUtil.create("div", 'info legend');
-      let item = "";
+  addLegend() {
+    this._mapService.legend = new L.Control({ position: 'bottomright' });
+    this._mapService.legend.onAdd = function () {
+      const div = L.DomUtil.create('div', 'info legend');
+      let item = '';
 
-      item += '<div id="LegendHeader" ><span><i class="fa fa-list"></i>Explanation</span></div>'+
+      item +=
+        '<div id="LegendHeader" ><span><i class="fa fa-list"></i>Explanation</span></div>' +
         '<div id="legendDiv"><br>';
-           item += '<i class="site multiple-types"></i>Multiple</div>';
-           div.innerHTML = item;
-           div.id = 'legend';
+      item += '<i class="site multiple-types"></i>Multiple</div>';
+      div.innerHTML = item;
+      div.id = 'legend';
 
-          //  L.DomEvent.on(div, 'click', (event) => {
-          //   // if click is in Explanation title, collapse/expand it.
-          //   const id = event.target['id'];
-          //   if ('legendHeader') {
-          //       const classes = document.getElementById('legendDiv').classList;
-          //       if (classes.contains('legendDiv-collapsed')) {
-          //           classes.remove('legendDiv-collapsed');
-          //       } else {
-          //           classes.add('legendDiv-collapsed');
-          //       }
-          //   }
-          // });
-          return div;
-    }
+      //  L.DomEvent.on(div, 'click', (event) => {
+      //   // if click is in Explanation title, collapse/expand it.
+      //   const id = event.target['id'];
+      //   if ('legendHeader') {
+      //       const classes = document.getElementById('legendDiv').classList;
+      //       if (classes.contains('legendDiv-collapsed')) {
+      //           classes.remove('legendDiv-collapsed');
+      //       } else {
+      //           classes.add('legendDiv-collapsed');
+      //       }
+      //   }
+      // });
+      return div;
+    };
     this._mapService.legend.addTo(this._mapService.map);
-  };
+  }
 
-// When data or map is collapsed or expanded,
-// invalidate size to refresh tiles and center map
+  // When data or map is collapsed or expanded,
+  // invalidate size to refresh tiles and center map
   // resizeMap() {
   //   setTimeout(() => {
   //       this._mapService.map.invalidateSize()
   //   }, 150);
   // }
-
 }
--- src/app/shared/services/map.service.ts
+++ src/app/shared/services/map.service.ts
@@ -1,15 +1,13 @@
-  
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { Map, GeoJSON, latLng, LatLng} from 'leaflet';
+import { Map, GeoJSON, latLng, LatLng } from 'leaflet';
 
 import 'leaflet';
 import * as esri from 'esri-leaflet';
 import * as L from 'leaflet';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MapService {
   public map!: Map;
@@ -22,24 +20,25 @@ export class MapService {
   public basinLayer!: any;
   public huc8Layer!: any;
   public overlayLayers: any;
+  public basinAreaStyle: any;
 
-//   public siteColors = ['red', 'blue', 'green', 'gray'];
-//   public siteCategories = ['Active', 'Suspected', 'Closed', 'Other']
+  //   public siteColors = ['red', 'blue', 'green', 'gray'];
+  //   public siteCategories = ['Active', 'Suspected', 'Closed', 'Other']
 
-//   public _selectedSiteSubject = new Subject();
-//   public get SelectedSite(): Observable<any> {
-//       return this._selectedSiteSubject.asObservable();
-//   }
+  //   public _selectedSiteSubject = new Subject();
+  //   public get SelectedSite(): Observable<any> {
+  //       return this._selectedSiteSubject.asObservable();
+  //   }
 
-//   public _selectedCanSiteSubject = new Subject();
-//   public get SelectedCanSite(): Observable<any> {
-//     return this._selectedCanSiteSubject.asObservable();
-//   }
+  //   public _selectedCanSiteSubject = new Subject();
+  //   public get SelectedCanSite(): Observable<any> {
+  //     return this._selectedCanSiteSubject.asObservable();
+  //   }
 
-//   public _selectedCanadaSiteSubject = new Subject();
-//   public get SelectedCanadaSite(): Observable<any> {
-//       return this._selectedCanadaSiteSubject.asObservable();
-//   }
+  //   public _selectedCanadaSiteSubject = new Subject();
+  //   public get SelectedCanadaSite(): Observable<any> {
+  //       return this._selectedCanadaSiteSubject.asObservable();
+  //   }
 
   public dataPanelCollapseSubject = new Subject();
   public get DataPanelCollapse(): Observable<any> {
@@ -49,34 +48,58 @@ export class MapService {
   constructor() {
     this.chosenBaseLayer = 'Topo';
 
-    this.baseMaps = {// {s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png  
-      OpenStreetMap: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        zIndex: 1,
-        maxZoom: 20,
-        attribution: 'Imagery from <a href="https://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }),
-      Topo: L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}", {
-        zIndex: 1,
-        attribution: "Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
-      }),
-      CartoDB: L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png", {
-        zIndex: 1,
-        attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='https://cartodb.com/attributions'>CartoDB</a>"
-      }),
-      Satellite: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'//,
-        //maxZoom: 10
-      }),
-      Terrain: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
-        maxZoom: 13,
-        zIndex: 1
-      }),
-      Gray: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-        maxZoom: 16,
-        zIndex: 1
-      }),
+    this.baseMaps = {
+      // {s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png
+      OpenStreetMap: L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          zIndex: 1,
+          maxZoom: 20,
+          attribution:
+            'Imagery from <a href="https://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }
+      ),
+      Topo: L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+        {
+          zIndex: 1,
+          attribution:
+            'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
+        }
+      ),
+      CartoDB: L.tileLayer(
+        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        {
+          zIndex: 1,
+          attribution:
+            "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='https://cartodb.com/attributions'>CartoDB</a>",
+        }
+      ),
+      Satellite: L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        {
+          attribution:
+            'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community', //,
+          //maxZoom: 10
+        }
+      ),
+      Terrain: L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}',
+        {
+          attribution:
+            'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
+          maxZoom: 13,
+          zIndex: 1,
+        }
+      ),
+      Gray: L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+        {
+          attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+          maxZoom: 16,
+          zIndex: 1,
+        }
+      ),
       // Nautical: esri.tiledMapLayer({
       //   url: 'https://seamlessrnc.nauticalcharts.noaa.gov/arcgis/rest/services/RNC/NOAA_RNC/ImageServer',
       //   zIndex: 1
@@ -86,42 +109,57 @@ export class MapService {
 
     this.basinLayer = {
       basin: esri.featureLayer({
-        url: 'https://services7.arcgis.com/Tk0IbKIKhaoYn5sa/ArcGIS/rest/services/GreatLakesCommissionBasinBoundary/FeatureServer/0'
-      })
+        url:
+          'https://services7.arcgis.com/Tk0IbKIKhaoYn5sa/ArcGIS/rest/services/GreatLakesCommissionBasinBoundary/FeatureServer/0',
+      }),
     };
     this.huc8Layer = {
       huc8: esri.dynamicMapLayer({
         url: 'https://hydro.nationalmap.gov/arcgis/rest/services/wbd/MapServer',
         layers: [4],
-        layerDefs: { 4: "HUC8 LIKE '04%'" }
-      })
+        layerDefs: { 4: "HUC8 LIKE '04%'" },
+      }),
     };
 
-    this.auxLayers = L.layerGroup([this.basinLayer, this.huc8Layer])
-    
+    this.auxLayers = L.layerGroup([this.basinLayer, this.huc8Layer]);
+
     this.overlayLayers = {
-      "Great Lakes Basin": this.basinLayer,
-      "HUC8 Subbasin": this.huc8Layer
+      'Great Lakes Basin': this.basinLayer,
+      'HUC8 Subbasin': this.huc8Layer,
+    };
+
+    this.basinAreaStyle = {
+      color: 'green',
+      fillOpacity: 0.25,
+      opacity: 0.65,
+      weight: 2,
     };
 
     this.auxLayers = {
-      basinArea: esri.dynamicMapLayer({
-        url: 'https://gis.wim.usgs.gov/arcgis/rest/services/SIGL/SIGLMapper/MapServer',
-        layers: [3],
-        pane: 'basins',
-        f: 'image',
-        opacity: .4
+      basinArea: esri.featureLayer({
+        url:
+          'https://gis.wim.usgs.gov/arcgis/rest/services/SIGL/SIGLMapper/MapServer/3',
+        style: this.basinAreaStyle,
       }),
       basin: esri.featureLayer({
-        url: 'https://services7.arcgis.com/Tk0IbKIKhaoYn5sa/ArcGIS/rest/services/GreatLakesCommissionBasinBoundary/FeatureServer/0'
+        url:
+          'https://services7.arcgis.com/Tk0IbKIKhaoYn5sa/ArcGIS/rest/services/GreatLakesCommissionBasinBoundary/FeatureServer/0',
       }),
       huc8: esri.dynamicMapLayer({
-        url: 'https://hydro.nationalmap.gov/arcgis/rest/services/wbd/MapServer',
-        layers: [4],
+        url:
+          'https://gis.streamstats.usgs.gov/arcgis/rest/services/StreamStats/nationalLayer/MapServer',
+        layers: [2, 3, 4, 5, 6, 7],
+        maxZoom: 14,
+        minZoom: 4,
+        useCors: false,
+      }),
+      /*
+      huc8: esri.featureLayer({
+        url:
+          'https://hydro.nationalmap.gov/arcgis/rest/services/wbd/MapServer/4',
+
         //layerDefs: { 4: "HUC8 LIKE '04%'" }
-      })
+      }), */
     };
-
-   }
-
-}//END Mapservice Class
+  }
+} //END Mapservice Class
