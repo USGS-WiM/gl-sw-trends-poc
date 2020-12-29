eae315c46c39d81d55625a81b78ec4aec248b784
09Kathy <kathymdooley@gmail.com>
2020-12-29 16:48:56 -0600

    Issue #28 - comments, renaming

--- src/app/mainview/sidebar/sidebar.component.ts
+++ src/app/mainview/sidebar/sidebar.component.ts
@@ -27,22 +27,25 @@ export class SidebarComponent implements OnInit {
     // this.onChanges();
   }
   // called from basemap button click in sidebar
-  public toggleLayer(selectedBase: string, selectedBaseId: string) {
+  public toggleBasemap(selectedBase: string, selectedBaseId: string) {
+    //get the element id of the previous basemap selection, and uncheck the radio button
     let oldRadioID = document.getElementById(
       this.chosenBaseId
     ) as HTMLInputElement;
     oldRadioID.checked = false;
+    //get the element id of the new basemap selection, and check the radio button
     let newRadioID = document.getElementById(
       selectedBaseId
     ) as HTMLInputElement;
     newRadioID.checked = true;
+    //remove the previous basemap layer
     this._mapService.map.removeLayer(
       this._mapService.baseMaps[this._mapService.chosenBaseLayer]
     );
     this._mapService.chosenBaseLayer = selectedBase;
+    //add the new basemap layer
     this._mapService.map.addLayer(this._mapService.baseMaps[selectedBase]);
     this.chosenBaseId = selectedBaseId;
-    console.log('chosenBaseId', this.chosenBaseId);
   }
 
   //called from aux layer button click in sidebar
@@ -64,7 +67,7 @@ export class SidebarComponent implements OnInit {
   }
 
   //when an Additional Layer is checked, add/remove that layer from the map
-  public toggleAuxLayer(mapLayer: string, layerID: string) {
+  public toggleMapLayer(mapLayer: string, layerID: string) {
     // this._mapService.chosenAuxLayer = newVal;
     let checkboxID = document.getElementById(layerID) as HTMLInputElement;
     if (checkboxID.checked == false) {
